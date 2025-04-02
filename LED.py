dfc38ad75fd243af033bbc46b8b0102f59bd42b5
Sherlock Furmalin <121876640+Sherlock0129@users.noreply.github.com>
2025-04-02 10:13:51 +0800

    Update LED.py

--- LED.py
+++ LED.py
@@ -1,10 +1,26 @@
-import RPi.GPIO as GPIO
-import time
+# NUIST in Python
+# Author: Dongxu Xia
+# Date: 2025/3/31
+# Description: A simple program to control an LED using Raspberry Pi GPIO
+
+# Import required libraries
+import RPi.GPIO as GPIO  # Library for GPIO control
+import time               # Library for time-related functions
+
+# Set GPIO mode to BCM numbering
 GPIO.setmode(GPIO.BCM)
+
+# Disable GPIO warnings
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print ("LED on")
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-print ("LED off")
-GPIO.output(18,GPIO.LOW)
+
+# Set GPIO pin 18 as output
+GPIO.setup(18, GPIO.OUT)
+
+# Turn LED on
+print("LED on")
+GPIO.output(18, GPIO.HIGH)  # Set pin 18 to HIGH (3.3V) to turn on LED
+time.sleep(1)               # Wait for 1 second
+
+# Turn LED off
+print("LED off")
+GPIO.output(18, GPIO.LOW)   # Set pin 18 to LOW (0V) to turn off LED
